8ab797a11ff22d3dc399e6ae3497d8f61e13d5d5
jromao <joao.romao@partner.criticaltechworks.com>
2024-07-04 16:45:40 +0100

    Small adjustments.

--- src/main/java/com/criticalsoftware/users/UserResource.java
+++ src/main/java/com/criticalsoftware/users/UserResource.java
@@ -16,6 +16,8 @@ import java.util.stream.Collectors;
 @Path("/users")
 public class UserResource {
 
+    public static final String USER_NOT_FOUND = "User not found.";
+
     @Inject
     UserRepository repository;
 
@@ -44,7 +46,7 @@ public class UserResource {
     public Response getByEmail(@PathParam("email") String email) {
         User user = repository.findByEmail(email);
         if (user == null) {
-            return Response.status(Response.Status.NOT_FOUND).entity("User not found.").build();
+            return Response.status(Response.Status.NOT_FOUND).entity(USER_NOT_FOUND).build();
         }
         UserResponse userResponse = new UserResponse(user.id, user.getName(), user.getDateBirth(), user.getContact());
         return Response.ok(userResponse).build();
@@ -55,7 +57,7 @@ public class UserResource {
     public Response getById(@PathParam("id") String id) {
         User user = repository.findById(new ObjectId(id));
         if (user == null) {
-            return Response.status(Response.Status.NOT_FOUND).entity("User not found.").build();
+            return Response.status(Response.Status.NOT_FOUND).entity(USER_NOT_FOUND).build();
         }
         UserResponse userResponse = new UserResponse(user.id, user.getName(), user.getDateBirth(), user.getContact());
         return Response.ok(userResponse).build();
@@ -101,7 +103,7 @@ public class UserResource {
             ObjectId objectId = new ObjectId(id);
             User user = repository.findById(objectId);
             if (user == null) {
-                return Response.status(Response.Status.NOT_FOUND).entity("User not found.").build();
+                return Response.status(Response.Status.NOT_FOUND).entity(USER_NOT_FOUND).build();
             }
             repository.delete(user);
             return Response.noContent().build();
